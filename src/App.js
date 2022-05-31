57aa2e9d0fa0202d337ba533f83c23a698e2a7ad
Satyam Rai <satyamrai0911@gmail.com>
2022-05-31 22:16:43 +0530

    starter code and configuration

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>This is upGrad E-shop</h1>
+      <h4>Checkpoint 1</h4>
+      <h4>Starter Code and Configuration Guidelines</h4>
+      <h5>
+        Successfully created react app (upGrad E-shop)and created folder
+        structure as per upGrad guidelines.
+      </h5>
+    </div>
+  );
+}
+
+export default App;
